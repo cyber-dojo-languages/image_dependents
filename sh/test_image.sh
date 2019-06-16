82b82e658c50fe0cf508a67d840216d1a137caff
Jon Jagger <jon@jaggersoft.com>
2019-06-16 20:59:03 +0100

    add simple test

---
+++ sh/test_image.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+readonly TMP_DIR=$(mktemp -d)
+
+remove_tmp_dir()
+{
+  rm -rf "${TMP_DIR}" > /dev/null;
+}
+
+trap remove_tmp_dir INT EXIT
+
+assert_equals()
+{
+  local -r expected="${1}"
+  local -r actual="${2}"
+  if [ "${expected}" != "${actual}" ]; then
+    echo 'FAILED'
+    echo "expected: ${expected}"
+    echo "  actual: ${actual}"
+    exit 3
+  fi
+}
+
+docker_run_image_dependents()
+{
+  docker run \
+    --rm \
+    --volume "${PWD}:/data:ro" \
+      cyberdojofoundation/image_dependents
+}
+
+cd ${TMP_DIR}
+git clone https://github.com/cyber-dojo-languages/python.git
+cd python
+EXPECTED="python-assert python-behave python-pytest python-unittest"
+ACTUAL=$(docker_run_image_dependents)
+assert_equals "${EXPECTED}" "${ACTUAL}"
+
+cd ${TMP_DIR}
+git clone https://github.com/cyber-dojo-languages/python-pytest.git
+cd python-pytest
+EXPECTED="csharp-moq csharp-nunit csharp-specflow"
+ACTUAL=$(docker_run_image_dependents)
+assert_equals "${EXPECTED}" "${ACTUAL}"
